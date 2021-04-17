d604f0eced5781d832448cdbe99c19aed3596445
aleksandrsg <aleksandrs.galiullins@gmail.com>
2021-04-17 18:50:22 +0100

    New Component Dialogs

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import Header from './components/Header/Header';
 import Nav from './components/Navbar/Nav';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
 
 const App = () => {
   return (
     <div className="wrapper"> 
       <Header />
       <Nav />
+      <div className="wrapper-content">
       <Profile />
+      </div>
     </div>
   );
 }
