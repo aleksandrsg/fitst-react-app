dc8ba13386527ba092c467d04da62dfa36fbb856
aleksandrsg <aleksandrs.galiullins@gmail.com>
2021-04-17 19:27:15 +0100

    Installed react-router-dom and created routes for dialogs and profile

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Header from './components/Header/Header';
 import Nav from './components/Navbar/Nav';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 const App = () => {
   return (
+    <BrowserRouter>
     <div className="wrapper"> 
       <Header />
       <Nav />
       <div className="wrapper-content">
-      <Profile />
+        <Route path='/dialogs' component={Dialogs}/>
+        <Route path = '/profile' component={Profile}/>
       </div>
     </div>
+    </BrowserRouter>
   );
 }
 
