6c95c2e51877c52df9b0e1f3b16c9b8b6fb671aa
aleksandrsg <aleksandrs.galiullins@gmail.com>
2021-04-25 21:27:38 +0100

    added arrays data for Dialogs and Messages

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -20,17 +20,34 @@ const Message =(props) =>{
 }
 
 const Dialogs = (props) => {
+
+    let dialogsData = [
+        {id: 1, name:'Olga'},
+        {id: 2, name:'Alex'},
+        {id: 3, name:'Leo'},
+        {id: 4, name:'Lada'},
+        {id: 5, name:'Anna'}
+    ]
+
+    let messagesData = [
+        {id:1, message:"Hi"},
+        {id:2, message:"How are you?"},
+        {id:3, message:"Well!"},
+    ]
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogItems}>
-                <DialogItem name='Olga' id='1'/>
-                <DialogItem name='Aleks' id='2'/>
-                <DialogItem name='Leo' id='3'/>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
             </div> 
             <div className={classes.messages}>
-                <Message message="Hi"/>
-                <Message message="How are you?"/>
-                <Message message="Well!"/>
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
+                <Message message={messagesData[2].message}/>
             </div>
         </div>
     );
