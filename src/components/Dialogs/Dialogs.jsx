365a1b7312fbb8b23e9cfb8a39f1b939fc487e34
aleksandrsg <aleksandrs.galiullins@gmail.com>
2021-05-03 17:20:52 +0100

    Aopplied MAP method for Posts, Messages, Dialogs Components

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -21,33 +21,30 @@ const Message =(props) =>{
 
 const Dialogs = (props) => {
 
-    let dialogsData = [
+    let dialogs = [
         {id: 1, name:'Olga'},
         {id: 2, name:'Alex'},
         {id: 3, name:'Leo'},
         {id: 4, name:'Lada'},
         {id: 5, name:'Anna'}
     ]
-
-    let messagesData = [
+    
+    let messages = [
         {id:1, message:"Hi"},
         {id:2, message:"How are you?"},
         {id:3, message:"Well!"},
     ]
 
+    let dialogsElements = dialogs.map( d => <DialogItem name={d.name} id={d.id}/>);
+    let messagesElements = messages.map( m => <Message message={m.message}/>)
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
-                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
+                {dialogsElements}
             </div> 
             <div className={classes.messages}>
-                <Message message={messagesData[0].message}/>
-                <Message message={messagesData[1].message}/>
-                <Message message={messagesData[2].message}/>
+                {messagesElements}
             </div>
         </div>
     );
