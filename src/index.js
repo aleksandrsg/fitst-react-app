3b3cb1b6ceb4dd2f5200ea04d1816a1655a7eafb
aleksandrsg <aleksandrs.galiullins@gmail.com>
2021-05-08 18:22:34 +0100

    Data stored in new file called state.js

--- src/index.js
+++ src/index.js
@@ -3,31 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-let posts = [
-  {id:1, message:"Hi this is my first Props", likes:15, dislikes:100},
-  {id:2, message:"I learn React.js", likes:100, dislikes:0},
-  {id:3, message:"Use .MAP method", likes:20, dislikes:2},
-]
-
-let dialogs = [
-  {id: 1, name:'Olga'},
-  {id: 2, name:'Alex'},
-  {id: 3, name:'Leo'},
-  {id: 4, name:'Lada'},
-  {id: 5, name:'Anna'}
-]
-
-let messages = [
-  {id:1, message:"Hi"},
-  {id:2, message:"How are you?"},
-  {id:3, message:"Well!"},
-]
-
+import state from './redux/state.js'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App posts = { posts } dialogs = {dialogs} messages = {messages}/>
+    <App state = { state }/>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/redux/state.js
@@ -0,0 +1,25 @@
+let state = {
+    profilePage:{
+        posts:[
+        {id:1, message:"Hi this is my first Props", likes:15, dislikes:100},
+        {id:2, message:"I learn React.js", likes:100, dislikes:0},
+        {id:3, message:"Use .MAP method", likes:20, dislikes:2},
+        ]
+    },
+    dialogsPage:{
+        dialogs:[
+            {id: 1, name:'Olga'},
+            {id: 2, name:'Alex'},
+            {id: 3, name:'Leo'},
+            {id: 4, name:'Lada'},
+            {id: 5, name:'Anna'}
+        ],
+        messages:[
+            {id:1, message:"Hi"},
+            {id:2, message:"How are you?"},
+            {id:3, message:"Well!"},
+          ],
+    }     
+};
+
+export default state;
