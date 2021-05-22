6d8fa0c4c16c840f2b11a05405c2082fa4b7f809
aleksandrsg <aleksandrs.galiullins@gmail.com>
2021-05-22 21:53:37 +0100

    new function Subscriber in state.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {rerenderEntireTree} from './render.js'
+import App from './App';
+import {addPost, subscribe, updateNewPostText} from './redux/state.js'
 import state from './redux/state.js'
 
+export let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App state = { state } addPost={addPost} updateNewPostText={updateNewPostText}/>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  }
+
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import { rerenderEntireTree } from "../render";
+let rerenderEntireTree = () => {
+    console.log ('State changed');
+}
 
 let state = {
     profilePage:{
@@ -26,7 +28,7 @@ let state = {
 };
 
 
-export let addPost = (postMessage) =>{
+export const addPost = (postMessage) =>{
     let newPost = {
         id:4,
         message: postMessage,
@@ -37,9 +39,13 @@ export let addPost = (postMessage) =>{
     rerenderEntireTree(state);
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
     state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
+}
+
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer;
 }
 
 export default state;
